31c4673cda930637316fc17756c417e82322064e
marikannan16 <capkannan16@gmail.com>
2025-01-21 17:47:25 +0530

    initial commit

--- src/ApplicantExample/ApplicantExample.jsx
+++ src/ApplicantExample/ApplicantExample.jsx
@@ -180,9 +180,9 @@ const Example = () => {
                                 {currentDocuments.map((doc, index) => (
                                     <li
                                         key={index}
-                                        className="flex justify-between items-center flex-wrap mb-2 p-2 border rounded"
+                                        className="flex flex-col justify-between lg:flex-row  items-center flex-wrap mb-2 p-2 border rounded"
                                     >
-                                        <span className='flex justify-between gap-4 items-center'>
+                                        <span className='flex flex-wrap flex-col lg:flex-row  justify-between gap-4 items-center '>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                             </svg>
